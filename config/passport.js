2990c7e60657400b0a61bca86793369c33339d90
Thien Bui <thienbui797@gmail.com>
2018-05-12 01:32:47 +1000

    passport

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ var models = require('../models');
 passport.use(new FacebookStrategy({
         clientID: '173105820046468',
         clientSecret: 'c39b5af000a187b5ea9ebed20b0c52a5',
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
+        callbackURL: "https://thawing-ravine-87510.herokuapp.com/auth/facebook/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         console.log(profile);
