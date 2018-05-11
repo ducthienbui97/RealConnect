cdf5a6d791be70e064c98a5b88a74c07abe9d2cd
harryparkdotio <harry@harrypark.io>
2018-05-11 20:00:31 +1000

    fe boilerplate

---
+++ fe/src/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="app">
+		<router-link to="/">index</router-link>
+		<router-view></router-view>
+	</div>
+</template>
+
+<style src="@/assets/main.styl" lang="stylus"></style>
+
+<script>
+	export default {
+		name: 'app'
+	};
+</script>
