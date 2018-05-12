4c91c00b428cd3f7beb9b977dfc9c024b8161fc7
Thien Bui <thienbui797@gmail.com>
2018-05-12 15:20:12 +1000

    restaurant

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import Card, { CardContent, CardMedia } from "material-ui/Card";
 import Typography from "material-ui/Typography";
 import StarRatings from "react-star-ratings";
-import { withStyles } from 'material-ui/styles';
-
+import { withStyles } from "material-ui/styles";
+import {
+  FacebookShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  EmailShareButton
+} from "react-share";
 const styles = theme => ({
   card: {
     display: "flex"
@@ -39,7 +44,7 @@ class Restaurant extends Component {
     console.log(this.props);
     const { classes, theme } = this.props;
     return (
-      <div style={{margin:2}}>
+      <div style={{ margin: 2 }}>
         <Card className={classes.card}>
           <CardMedia
             className={classes.cover}
@@ -47,7 +52,7 @@ class Restaurant extends Component {
             title="Live from space album cover"
             style={{ width: 180, height: 180 }}
           />
-          <div  className={classes.details}>
+          <div className={classes.details}>
             <CardContent>
               <Typography variant="Display 1">{this.props.name}</Typography>
               <Typography variant="Display 2">{this.props.address}</Typography>
@@ -57,15 +62,16 @@ class Restaurant extends Component {
                 widgetDimensions="30px"
                 widgetSpacings="10px"
               />
+              <FacebookShareButton url={this.props.url} />
+              <TelegramShareButton url={this.props.url} />
+              <WhatsappShareButton url={this.props.url} />
+              <EmailShareButton url={this.props.url} />
             </CardContent>
           </div>
-
         </Card>
       </div>
     );
   }
 }
 
-
 export default withStyles(styles, { withTheme: true })(Restaurant);
-
